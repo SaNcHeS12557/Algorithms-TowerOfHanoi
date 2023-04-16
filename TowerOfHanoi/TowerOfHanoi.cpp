7752cb22006505ef1f46f456639b6a5e64474094
Oleksandr Shvarchevskyi <so53937@zut.edu.pl>
2023-04-16 17:30:58 +0200

    Recursive Hanoi

--- TowerOfHanoi/TowerOfHanoi.cpp
+++ TowerOfHanoi/TowerOfHanoi.cpp
@@ -4,15 +4,28 @@
 #include <iostream>
 using namespace std;
 
-int Hanoi(int n, int start_rod[], int free_rod[], int end_rod[]) {
-
-};
-
+void Hanoi(int n, int start_rod[], int free_rod[], int end_rod[])
+{
+    if (n != 0) {
+        Hanoi(n - 1, start_rod, end_rod, free_rod);
+        end_rod[n - 1] = start_rod[n - 1];
+        start_rod[n - 1] = 0;
+        Hanoi(n - 1, free_rod, start_rod, end_rod);
+    }
+}
+ 
 int main()
 {
-    const int n = 5; // number of discs
-    int A[n] = { 5, 4, 3, 2, 1 }, B[n], C[n]; // towers
+    const unsigned int n = 8; // number of discs
+    int A[n], B[n], C[n]; // towers
+
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = n - i;
+    }
+
     Hanoi(n, A, B, C); // calling the function
+
     for (int i = 0; i < n; i++)
     {
         cout << C[i] << " "; // cout'ing C tower
