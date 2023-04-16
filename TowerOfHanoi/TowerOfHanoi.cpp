6f8ad50eac594c3f42b9311313a89527a963a76d
Oleksandr Shvarchevskyi <so53937@zut.edu.pl>
2023-04-16 15:31:29 +0200

    Preparing Code

--- TowerOfHanoi/TowerOfHanoi.cpp
+++ TowerOfHanoi/TowerOfHanoi.cpp
@@ -2,19 +2,19 @@
 //
 
 #include <iostream>
+using namespace std;
+
+int Hanoi(int n, int start_rod[], int free_rod[], int end_rod[]) {
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int n = 5; // number of discs
+    int A[n] = { 5, 4, 3, 2, 1 }, B[n], C[n]; // towers
+    Hanoi(n, A, B, C); // calling the function
+    for (int i = 0; i < n; i++)
+    {
+        cout << C[i] << " "; // cout'ing C tower
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
